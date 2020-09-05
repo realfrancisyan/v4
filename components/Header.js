6f5b5669de7c57cae321323c9e8e62d939efaae8
Jiajun Yan <domo36k@gmail.com>
2020-09-05 11:30:42 +0800

    feat: Add css standards and variables

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import styles from '../styles/Header.module.css';
 import Logo from '../assets/images/logo.svg';
+import GitHubIcon from '../assets/images/github.svg';
+import InstagramIcon from '../assets/images/instagram.svg';
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <img src={Logo} className={styles.logo} />
+
+      <ul className={styles.links}>
+        <li>
+          <a href="https://github.com/realfrancisyan">
+            <img src={GitHubIcon} />
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/realfrancisyan">
+            <img src={InstagramIcon} />
+          </a>
+        </li>
+      </ul>
     </header>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 import ArrowIcon from '../assets/images/arrow.svg';
+import Background from '../assets/images/background.jpg';
+import CurveIcon from '../assets/images/curve.png';
+
 
 const portalList = [
   {
@@ -34,9 +37,31 @@ export default function Home() {
     <React.Fragment>
       <Header />
 
+      <section className={styles.project}>
+        <h2 className={styles.sectionTitle}>近期项目</h2>
+        <div className={styles.projectWrapper}>
+          <img src={Background} className={styles.projectImage} />
+          <div className={styles.projectTitleWrapper}>
+            <h2>
+              <img src={CurveIcon} className={styles.projectTitle} />
+            </h2>
+            <p className={styles.projectSubTitle}>简单易用、可自托管的小型 Serverless 服务</p>
+            <a href="/" className={styles.projectLink}>
+              在 GitHub 查看项目
+            </a>
+          </div>
+        </div>
+        <div className={styles.projectDesc}>
+          身认称报导别里则面切样，周技好地增过适张平，传劳霸响点抄专候弦。
+          持管明但收面代万种间，着义他共参结参速地，消会居我果义府问。
+          成史术们重其光次用京存，感上运个小海水选同，常酸屈场别丽伯决赤。
+          白那半起北关地位况矿，生非是大阶金以最，族向束志皂来住眼。
+        </div>
+      </section>
+
       <section className={styles.portal}>
         <div className={styles.portalContainer}>
-          <h2 className={styles.portalTitle}>我的想法</h2>
+          <h2 className={styles.sectionTitle}>我的想法</h2>
           <ul className={styles.portalListContainer}>
             {portalList.map((portal) => {
               return (
