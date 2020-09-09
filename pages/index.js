0ebb1c3473d29c29363366d96af05a3e134f5f3b
Jiajun Yan <domo36k@gmail.com>
2020-09-09 16:22:53 +0800

    refactor: Remove files that are no longer needed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
