1e1a8a9cb96d5a8a2bae262f614c87b340a6490d
Jiajun Yan <domo36k@gmail.com>
2020-09-12 12:44:27 +0800

    feat: Add nProgress support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
